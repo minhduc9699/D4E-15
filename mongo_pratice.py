6a7f16d844c360bac057db495c1c9ee4992575b0
Phạm Đức <minhduc.096.99@gmail.com>
2020-09-15 19:39:13 +0700

    .

--- mongo_pratice.py
+++ mongo_pratice.py
@@ -1,17 +1,27 @@
-from mongo_connect import mongo_connect
+import pymysql
 
-quiz_collection = mongo_connect()
+driver = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='@gmail.com',
+    cursorclass=pymysql.cursors.DictCursor
+)
 
-quizzes = quiz_collection.find()
+cursor = driver.cursor()
+
+cursor.execute('SELECT * FROM `d4e15`.`quiz`')
+quizzes = cursor.fetchall()
 
 for quiz in quizzes:
-    print(quiz['question'])
-    choices = quiz['choices']
-    for i in range(len(choices)):
-        print(f'{i+1}.{choices[i]}')
+    cursor.execute(f'SELECT * FROM `d4e15`.`choice` WHERE quiz_id = {quiz["id"]}')
+    choices = cursor.fetchall()
+    if len(choices) != 0:
+        print(quiz['question'])
+        for i in range(len(choices)):
+            print(f'{i+1}.{choices[i]['content']}')
 
-    user_choice = int(input('enter your guest')) - 1
-    if user_choice == quiz['awnser']:
-        print('uh')
-    else:
-        print('nahhh')
+        user_choice = int(input('enter your guest')) - 1
+        if user_choice == quiz['awnser']:
+            print('uh')
+        else:
+            print('nahhh')
